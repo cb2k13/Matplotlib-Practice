0917e0a11ceb4849cf38a3237dd54d8ad74825fd
Carlos Briceno <carlosbriceno33.2@gmail.com>
2025-09-22 01:58:08 -0700

    Initial Commit

---
+++ mpl_squares.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt # type: ignore
+
+# Create the list with data 
+squares = [1, 4, 9, 16, 25]
+
+# Call subplots() funcion
+# fig represents entire figure and ax represents single plot
+# in the figure 
+fig, ax = plt.subplots()
+
+ax.plot(squares, linewidth=3)
+
+# Set chart title and label axises
+ax.set_title("Square Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of value", fontsize=14)
+
+# Set size of tick labels 
+ax.tick_params(labelsize=14)
+
+# function to show and display the plot 
+plt.show()
